7983bbe20b36d457fc3253d17ca10a1f8ca5618e
ShanmukhK36 <katakamshanmukh@gmail.com>
2025-04-26 15:54:10 -0500

    updated userseed file

--- userSeed.js
+++ userSeed.js
@@ -5,17 +5,23 @@ import connectToDatabase from "./db/db.js";
 const userRegister = async () => {
     connectToDatabase()
     try {
-        const hashPassword = await bcrypt.hash("admin", 10)
-        const newUser = new User({
+        const admin = await User.findOne({ email: "admin@gmail.com" });
+        if (!admin) {
+          const hashPassword = await bcrypt.hash("admin", 10);
+          const newUser = new User({
             name: "Admin",
             email: "admin@gmail.com",
             password: hashPassword,
             role: "admin"
-        })
-        await newUser.save()
-    } catch(error) {
-        console.log(error.message);
-    }
+          });
+          await newUser.save();
+          console.log("Admin user created!");
+        } else {
+          console.log("Admin user already exists.");
+        }
+      } catch (error) {
+        console.log("Error creating admin user:", error);
+      }
 }
 
 export {userRegister}
